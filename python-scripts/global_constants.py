7373e9a914572be46a84f7e35f31e0d6598c6471
Sorahawk <sorahawktech@gmail.com>
2022-08-27 18:17:48 +0800

    removed a couple of armor keywords because of generic reference to body parts which could refer to insect parts as well; left just those words which are mainly for clothing pieces

--- python-scripts/global_constants.py
+++ python-scripts/global_constants.py
@@ -148,9 +148,9 @@ SPECIAL_WORDS = ['AARTZ', 'BBQ', 'BLT', 'BURG.L', 'EverChar', 'HQ', 'a', 'and', 
 
 # lists of words that correspond to each armor piece type
 ARMOR_KEYWORDS = {
-	'Head': ['face', 'hat', 'head', 'helmet', 'hood', 'mask'],
-	'Upper Body': ['arm', 'chest', 'shirt', 'shoulder', 'torso', 'vest'],
-	'Lower Body': ['boots', 'knee', 'leg', 'pants', 'shin', 'shoes']
+	'Head': ['face', 'hat', 'helmet', 'hood', 'mask'],
+	'Upper Body': ['chest', 'shirt', 'shoulder', 'torso', 'vest'],
+	'Lower Body': ['boots', 'knee', 'pants', 'shin', 'shoes']
 }
 
 
