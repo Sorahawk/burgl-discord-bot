6d465e810b99a8719c2a67252b80846eb028808e
Sorahawk <sorahawktech@gmail.com>
2025-05-03 09:39:05 +0800

    optimised check_flags using dictionary comprehension

--- python-scripts/string_processing.py
+++ python-scripts/string_processing.py
@@ -26,23 +26,15 @@ def check_command(user_input):
 # first, a dictionary of booleans indicating presence of command flags
 # second, the user input stripped of flags
 def check_flags(user_input):
-	flag_presence = {}
-
 	# insert surrounding whitespace so leading and trailing flags can still be detected
 	user_input = f' {user_input} '
 
-	for flag_key, flag in BOT_COMMAND_FLAGS.items():
-		flag_presence[flag_key] = False
-		flag = f' -{flag} '  # flag must be standalone with surrounding whitespace
-
-		if flag in user_input.lower():
-			flag_presence[flag_key] = True
+	# generate flag presence dictionary
+	flag_presence = {flag: True if f' -{letter} ' in user_input.lower() else False for flag, letter in BOT_COMMAND_FLAGS.items()}
 
 	# remove all 'flags', a dash followed by a single letter, even if they are not valid
-	all_flags = ' -[a-z] '
-
-	# duplicate each whitespace within the input so that each present flag can be matched by the regex properly
-	user_input = re.sub(all_flags, ' ', ' ' + user_input.replace(' ', '  ') + ' ', flags=re.IGNORECASE)
+	# each whitespace within input is duplicated so that all present flags can be matched by the regex properly
+	user_input = re.sub(' -[a-z] ', ' ', ' ' + user_input.replace(' ', '  ') + ' ', flags=re.IGNORECASE)
 
 	# remove excess whitespace
 	user_input = ' '.join(user_input.split())
